4c474c1a8df7278999eb909905a41c5d866bc221
Noe A <neseiza@gmail.com>
2017-04-30 23:40:25 -0500

    landing added

--- src/main/java/com/simopuve/helper/WriteAndReadExcel.java
+++ src/main/java/com/simopuve/helper/WriteAndReadExcel.java
@@ -15,11 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
