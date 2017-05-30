ab0b80630a06ac917b11828d2c9adaa1cfd4d2b2
aldoram5 <master-sword5al@hotmail.com>
2017-05-30 02:38:53 -0500

    ...

--- src/main/java/com/simopuve/rest/SimopuveRESTServices.java
+++ src/main/java/com/simopuve/rest/SimopuveRESTServices.java
@@ -192,10 +192,10 @@ public class SimopuveRESTServices {
                 final String[] values = credentials.split(":", 2);
                 if (values.length > 0 && values[0].contentEquals("prueba") && values[1].contentEquals("prueba")) {
 
-                    return Response.status(200).entity(new PDVHeader("Terapaca", "dirección chilena", "comuna bonita", 0, 0, 0, new Date(), "Usuario Pruebas", 0)).build();
+                    return Response.status(200).entity(new PDVHeader("Terapaca", "dirección de prueba", "comuna de prueba", 0, 0, 0, new Date(), "Usuario Pruebas", 0)).build();
                 }if (values.length > 0 && values[0].contentEquals("Prueba") && values[1].contentEquals("Prueba")) {
 
-                    return Response.status(200).entity(new PDVHeader("Terapaca", "dirección chilena", "comuna bonita", 0, 0, 0, new Date(), "Usuario Pruebas", 0)).build();
+                    return Response.status(200).entity(new PDVHeader("Terapaca", "dirección de prueba", "comuna de prueba", 0, 0, 0, new Date(), "Usuario Pruebas", 0)).build();
                 }
                 if(logins.containsKey(credentials)){
                     return Response.status(200).entity(logins.get(credentials)).build();
