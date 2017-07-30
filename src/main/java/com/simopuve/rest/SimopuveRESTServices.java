6dc9f4c18f893905ded0de4a16f211100a86f8a6
Noe A <neseiza@gmail.com>
2017-07-30 13:19:13 -0500

    Yet more users

--- src/main/java/com/simopuve/rest/SimopuveRESTServices.java
+++ src/main/java/com/simopuve/rest/SimopuveRESTServices.java
@@ -83,9 +83,16 @@ public class SimopuveRESTServices {
             put("sebastian.prado.enth:YFpzZ78x", new PDVHeader("ENTEL HUERFANOS", "Huerfanos N° 1025 - SANTIAGO", "SANTIAGO", 0, 0, 0, new Date(), "Sebastian Prado", 0, false));
             put("carla.f:1155yQNl", new PDVHeader("TALCA CLARO", "8 Oriente Nº 1164 (Movired)", "TALCA", 0, 0, 0, new Date(), "Carla Figueroa ", 0, false));
             put("guille.s:5356jtTA", new PDVHeader("TALCA ENTEL","1 Sur Nº 1271 (entre 5 y 6 oriente) ","TALCA" ,0, 0, 0, new Date(), "Guillermo Stull", 0, false));
-            put("maria.d:9715ZLmq", new PDVHeader("TALCA MOVISTAR","1 Sur Nº 1170 (entre 4 y 5 oriente)","TALCA" ,0, 0, 0, new Date(), "María Duran ", 0, false));    
+            put("maria.d:9715ZLmq", new PDVHeader("TALCA MOVISTAR","1 Sur Nº 1170 (entre 4 y 5 oriente)","TALCA" ,0, 0, 0, new Date(), "Maria Duran ", 0, false));    
             put("carla.s:6116lFbo", new PDVHeader("TALCA Wom","1 SUR 1279","TALCA" ,0, 0, 0, new Date(), "Carla Stull", 0, false));
-            put("", new PDVHeader("","","" ,0, 0, 0, new Date(), "", 0, false));
+            put("lorenza.b:1009BXKn", new PDVHeader("CLARO CONCEPCION","O\'Higgins N 796","CONCEPCION" ,0, 0, 0, new Date(), "Lorenza Barra", 0, false));
+            put("gladys.h:4137kpqw", new PDVHeader("ENTEL CONCEPCION","O\'Higgins N 824","CONCEPCION" ,0, 0, 0, new Date(), "Gladys Herrera", 0, false));            
+            put("pablo.g:1871Axed", new PDVHeader("MOVISTAR CONCEPCION","O\'Higgins N 674","CONCEPCION" ,0, 0, 0, new Date(), "Pablo Gonzalez", 0, false));
+            put("macarena.a:5620LseT", new PDVHeader("Wom CONCEPCION","O\'Higgins N ","CONCEPCION" ,0, 0, 0, new Date(), "Macarena Araneda", 0, false));
+            put("rosa.v:7527xEZf", new PDVHeader("CLARO PUERTO MONTT","Talca N 141","PUERTO MONTT" ,0, 0, 0, new Date(), "Rosa Venegas", 0, false));
+            put("claudio.m:5098UAKt", new PDVHeader("ENTEL PUERTO MONTT","Varas N 515","PUERTO MONTT" ,0, 0, 0, new Date(), "Claudio Manzanarez", 0, false));
+            put("maria.m:3612ZJjv", new PDVHeader("Wom PUERTO MONTT","Guillermo Gallardo N 170","PUERTO MONTT" ,0, 0, 0, new Date(), "Maria Marquez", 0, false));
+            put("nicolas.m:7337NhMR", new PDVHeader("TMM PUERTO MONTT","PEDRO MONTT 120","PUERTO MONTT" ,0, 0, 0, new Date(), "Nicolas Mansanarez", 0, false));
             put("root:123456", new PDVHeader("", "", "", 0, 0, 0, new Date(), "", 0, false));
         }
     };
